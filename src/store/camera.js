0db89d0d98c68355307bf17bfb9870836e980d50
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2020-04-08 22:10:45 +0200

    add support for moving the camera up/down

--- src/store/camera.js
+++ src/store/camera.js
@@ -1,24 +1,41 @@
-const CAMERA_MARGIN = 128;
-
-
 function updatePosition(camera, game, masza) {
   // Move camera left
   if(
-    masza.pos.x - camera.pos.x < CAMERA_MARGIN &&
+    masza.pos.x - camera.pos.x < camera.width/4 &&
     camera.pos.x > 0
   ) {
     camera.pos.x = Math.max(
-      masza.pos.x - CAMERA_MARGIN,
+      masza.pos.x - camera.width/4,
       0
     )
   // Move camera right
   } else if(
-    masza.pos.x > camera.pos.x + camera.width - CAMERA_MARGIN &&
+    masza.pos.x > camera.pos.x + camera.width - camera.width/4 &&
     camera.pos.x < game.width - camera.width
   ) {
     camera.pos.x = Math.min(
-      masza.pos.x - camera.width + CAMERA_MARGIN,
+      masza.pos.x - camera.width + camera.width/4,
       game.width - camera.width
+    )
+  }
+
+  // Move camera up
+  if(
+    masza.pos.y - camera.pos.y < camera.height/4 &&
+    camera.pos.y > 0
+  ) {
+    camera.pos.y = Math.max(
+      masza.pos.y - camera.height/4,
+      0
+    )
+  // Move camera down
+  } else if(
+    masza.pos.y > camera.pos.y + camera.height - camera.height/4 &&
+    camera.pos.y < game.height - camera.height
+  ) {
+    camera.pos.y = Math.min(
+      masza.pos.y - camera.height + camera.height/4,
+      game.height - camera.height
     )
   }
 }
